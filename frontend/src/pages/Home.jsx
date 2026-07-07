8700a572e6af80418f1d8fe642544c2cb30ab24c
Chamuditha Tennakoon <chamu4442@live.com>
2026-07-07 19:15:38 +0530

    loading animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Header from "../components/Header.jsx";
 export default function Home() {
     const [products, setProducts] = useState([]);
     const { addToCart } = useContext(CartContext);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchProducts();
@@ -15,24 +16,32 @@ export default function Home() {
     const fetchProducts = async () => {
         const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/sarees`);
         setProducts(res.data);
+        setLoading(false);
     }
 
     return (
         <>
-        <Header/>
-        <div className="home-main">
-            <h1>Saree Collection</h1>
-                <div>
-                    {products.map(p => (
-                        <div key={p._id} >
-                            <img src={p.image} alt="" />
-                            <h2>{p.name}</h2>
-                            <p><b>Rs.{p.price}/=</b></p>
-                            <button onClick={() => addToCart(p)}>Add to Cart</button>
-                        </div>
-                    ))}
-                </div>
-        </div>
+            <Header />
+            <div className="home-main">
+                <h1>Saree Collection</h1>
+                {loading ? (
+                    <div className="loader-container">
+                        <div className="loader"></div>
+                        <p>Loading Sarees...</p>
+                    </div>
+                ) : (
+                    <div className="products">
+                        {products.map(p => (
+                            <div key={p._id} >
+                                <img src={p.image} alt="" />
+                                <h2>{p.name}</h2>
+                                <p><b>Rs.{p.price}/=</b></p>
+                                <button onClick={() => addToCart(p)}>Add to Cart</button>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </>
     );
 }
