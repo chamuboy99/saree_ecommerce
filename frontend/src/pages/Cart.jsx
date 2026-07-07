473f26123a81e3c385e94075230b102b1ae0a205
Chamuditha Tennakoon <chamu4442@live.com>
2026-07-08 01:07:20 +0530

    frontend update

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -12,26 +12,36 @@ export default function Cart() {
 
     return (
         <>
-            <Header/>
+            <Header />
             <div className="cart-main">
                 <h1>Cart</h1>
-                {cart.length === 0 ? <p>Cart is empty</p> : cart.map(item => (
-                    <div className="cart-item"  key={item._id}>
-                        <img src={item.image} alt="" />
-                        <div className="cart-details">
-                            <p>{item.name}</p>
-                            <p>Quantity: {item.quantity}</p>
-                            <p>Rs. {item.price}</p>
-                            <p>Subtotal: Rs. {item.quantity * item.price}</p>
-                        </div>
-                        <button onClick={() => removeFromCart(item._id)}>Remove</button>
-                    </div>
-                ))}
 
-                <h2>Total: Rs.{total}</h2>
+                {cart.length === 0 ? (
+                    <p>Cart is empty</p>
+                ) : (
+                    <>
+                        {cart.map(item => (
+                            <div className="cart-item" key={item._id}>
+                                <img src={item.image} alt={item.name} />
+                                <div className="cart-details">
+                                    <p>{item.name}</p>
+                                    <p>Quantity: {item.quantity}</p>
+                                    <p>Rs. {item.price}</p>
+                                    <p>Subtotal: Rs. {item.quantity * item.price}</p>
+                                </div>
+                                <button onClick={() => removeFromCart(item._id)}>
+                                    Remove
+                                </button>
+                            </div>
+                        ))}
 
-                <button onClick={() => navigate('/checkout')}>Checkout</button>
+                        <h2>Total: Rs. {total}</h2>
 
+                        <button onClick={() => navigate("/checkout")}>
+                            Checkout
+                        </button>
+                    </>
+                )}
             </div>
         </>
     );
