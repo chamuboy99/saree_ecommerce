421e0b34d073bb2e63484a6b0729b5027521b0a9
Chamuditha Tennakoon <chamu4442@live.com>
2026-07-16 02:32:54 +0530

    sidebar admin

--- admin/src/components/CategoryFilter.jsx
+++ admin/src/components/CategoryFilter.jsx
@@ -1,17 +1,60 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import '../styles/categoryfilter.css';
 import { FilterContext } from '../contexts/FilterContext.jsx';
+import { categories } from '../constants/cats.js';
 
 export default function CategoryFilter() {
     const { showSideBar, setShowSideBar } = useContext(FilterContext);
-    
-    if(!showSideBar) return null;
+    const [expanded, setExpanded] = useState(null);
 
-    return(
+    if (!showSideBar) return null;
+
+    const toggleCategory = (index) => {
+        setExpanded(expanded === index ? null : index);
+    }
+
+    return (
         <aside className={`filter-panel ${showSideBar ? "open" : ""}`}>
             <div>
-                <h1>Side Bar</h1>
+                <h2>Categories</h2>
             </div>
+            {categories.map((category, index) => (
+                <div key={category.name} className='category-item'>
+                    <div className='category-row'>
+                        <button className='category-name'>
+                            {category.name}
+                        </button>
+                        {category.subCategories && (
+                            <button className={`expand-btn ${expanded === index ? "rotate" : ""}`} onClick={() => toggleCategory(index)}>
+                                ▼
+                            </button>
+                        )}
+                    </div>
+
+                    {expanded === index && category.subCategories && (
+                        <div className='subcategory-list'>
+                            {category.subCategories.map(sub => (
+                                <div key={sub.name} className='subcategory-item'>
+                                    <button className='subcategory-row'>
+                                        {sub.name}
+                                    </button>
+                                    {sub.subSubCategories && (
+                                        <div className='subsubcategory-list'>
+                                            {sub.subSubCategories.map(s => (
+                                                <div key={s}>
+                                                    <button>
+                                                        {s}
+                                                    </button>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )}
+                                </div> 
+                            ))}
+                        </div>
+                    )}
+                </div>
+            ))}
         </aside>
     );
 }
---
+++ admin/src/constants/cats.js
@@ -0,0 +1,63 @@
+export const categories = [
+    {
+        name: "All"
+    },
+    {
+        name: "Handloom",
+        subCategories: [
+            {
+                name: "Rich Cotton"
+            },
+            {
+                name: "Rayon"
+            },
+            {
+                name: "Embroider"
+            },
+            {
+                name: "Puni"
+            }
+        ]
+    },
+
+    {
+        name: "Batik",
+        subCategories: [
+            {
+                name: "Normal Oil",
+                subSubCategories: [
+                    "2 Color",
+                    "3 Color"
+                ]
+            },
+            {
+                name: "Super Oil",
+                subSubCategories: [
+                    "3 Color"
+                ]
+            },
+            {
+                name: "Java"
+            }
+        ]
+    },
+
+    {
+        name: "Cotton",
+        subCategories: [
+            {
+                name: "Screen Printing"
+            },
+            {
+                name: "Lace Cotton Embroider"
+            },
+            {
+                name: "Super Voil Embroider"
+            }
+        ]
+    },
+
+    {
+        name: "Best Sellers"
+    }
+];
